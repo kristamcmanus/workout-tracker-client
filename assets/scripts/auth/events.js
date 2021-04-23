e748f85936b4216b4dfc277604573d57bbee2ba3
kristamcmanus <k.mcmanus310@gmail.com>
2021-04-23 10:43:05 -0400

    Add auth folder to scripts

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,75 @@
+'use strict'
+// import the functions that make requests to the api
+const api = require('./api')
+// import the functions that update our webpage's content
+const ui = require('./ui')
+// import the getFormFields function
+const getFormFields = require('../../../lib/get-form-fields')
+
+const store = require('../store')
+
+const onSignUp = function (event) {
+  // prevents page from refreshing when button is clicked
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  console.log(formData)
+  api.signUp(formData)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onError)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  console.log(formData)
+  api.signIn(formData)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onError)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.changePassword(formData)
+    .then(ui.onChangePasswordSuccess)
+    .catch(ui.onError)
+}
+
+const onSignOut = function () {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onError)
+}
+
+const onAddWorkout = function () {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  // console.log(formData)
+  api.addWorkout()
+    .then(ui.onAddWorkoutSuccess)
+    .catch(ui.onError)
+}
+
+const onAllWorkoutsTable = function () {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  // console.log(formData)
+  api.AllWorkoutsTable()
+    .then(ui.onAddWorkoutSuccess)
+    .catch(ui.onError)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut,
+  onAddWorkout,
+  onAllWorkoutsTable
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const store = require('../store')
+
+const onSignUpSuccess = function () {
+  $('#messages').text('Thanks for signing up!')
+  setTimeout(() => {
+    $('#messages').text('')
+  }, 3000)
+  $('#sign-up').trigger('reset')
+  $('.navbar').hide()
+}
+
+const onSignInSuccess = function (response) {
+  store.user = response.user
+  $('#messages').text('Successfully signed in!')
+  $('#change-password').show()
+  $('#sign-out').show()
+  $('.navbar').show()
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+  $('#sign-in').trigger('reset')
+}
+
+const onChangePasswordSuccess = function () {
+  $('#messages').text('Password changed successfully!')
+  $('#change-password').trigger('reset')
+}
+
+const onSignOutSuccess = function () {
+  $('#messages').text('Signed out successfully!')
+  $('#sign-out').hide()
+  $('#change-password').hide()
+  $('#sign-up').show()
+  $('#sign-in').show()
+  $('.navbar').hide()
+  store.user = null
+}
+
+const onNavbarSuccess = function () {
+  $('#add-workout').show()
+}
+
+const onAddWorkoutSuccess = function () {
+  // store.user = response.user
+  $('#messages').text('Successfully added workout!')
+  $('#add-workout').show()
+  $('#add-workout').trigger('reset')
+}
+
+const onAllWorkoutsTableSuccess = function () {
+  // store.user = response.user
+  $('#messages').text('You can now view all workouts!')
+  $('#all-workouts-table').show()
+}
+
+// displays error message
+const onError = function (err) {
+  console.error(err)
+  $('#messages').text('Hmm...something went wrong. Please try again!')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignInSuccess,
+  onChangePasswordSuccess,
+  onSignOutSuccess,
+  onAddWorkoutSuccess,
+  onAllWorkoutsTableSuccess,
+  onError
+}
