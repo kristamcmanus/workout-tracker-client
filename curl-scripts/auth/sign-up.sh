616054606e4dd6b5d8167ced1122f3b5887ea93b
kristamcmanus <k.mcmanus310@gmail.com>
2021-04-23 15:48:55 -0400

    Change url to heroku link

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,6 +1,6 @@
 # VARIABLE=VALUE sh curl-scripts/auth/sign-up.sh
 
-curl "http://localhost:4741/sign-up" \
+curl "https://safe-headland-27184.herokuapp.com" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
