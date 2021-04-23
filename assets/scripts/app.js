91f9440e6cb77ef9e7c68e805c5642a0b73e580a
kristamcmanus <k.mcmanus310@gmail.com>
2021-04-23 10:41:25 -0400

    Add navbar, add workouts and view all workouts to file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,23 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const events = require('./auth/events')
 
 $(() => {
-  // your JS code goes here
+  $('#change-password').hide()
+  $('#sign-out').hide()
+  $('#sign-up').on('submit', events.onSignUp)
+  $('#sign-in').on('submit', events.onSignIn)
+  $('#change-password').on('submit', events.onChangePassword)
+  $('#sign-out').on('click', events.onSignOut)
+  $('.navbar').on('click', events.onSignIn)
+  $('.navbar').hide()
+  // $('#all-workouts-table').hide()
+  $('#all-workouts-table').on('click', events.onAllWorkoutsTable)
+  // $('#add-workout').on('click', events.onNavbarSuccess)
+  $('#add-workout').on('click', events.onAddWorkout)
+  // $('#add-workout').hide()
+  // $('#add-workout').on('click', events.onSignIn)
+  // $('#all-workouts-table').hide()
+  // $('#all-workouts-table').on('click', events.onSignIn)
 })
